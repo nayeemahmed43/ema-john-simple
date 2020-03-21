e3e1f0d19818b373920d9e9e725d8cb0f01a726e
unknown <you@example.com>
2020-03-21 22:28:30 +0600

    auth integration

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Header/Shop/Shop';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Review from './components/Review/Review';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
+import ProductDetail from './components/ProductDetail/ProductDetail';
+import Login from './components/Login/Login';
+import { AuthContextProvider, PrivateRoute } from './components/Login/useAuth';
+import Shipment from './components/Shipment/Shipment';
 
 function App() {
+  const user = {name:'Nayeem',email:'jhjhhgajkjd.com'}
   return (
     <div>
+      <AuthContextProvider>
       <Header></Header>
-      <Shop></Shop>
+      <Router>
+        <Switch>
+
+          <Route path="/shop">
+             <Shop></Shop>
+          </Route>
+
+          <Route path="/review">
+              <Review></Review>
+          </Route>
+
+          <Route path="/inventory">
+              <Inventory></Inventory>
+          </Route>
+
+          <Route exact path="/">
+              <Shop></Shop>
+          </Route>
+
+          <Route path="/product/:productKey">
+              <ProductDetail></ProductDetail>
+          </Route>
+
+          <Route path="/login">
+              <Login></Login>
+          </Route>
+
+          <PrivateRoute path="/shipment">
+              <Shipment></Shipment>
+          </PrivateRoute>
+
+          <Route path="*">
+              <NotFound></NotFound>
+          </Route>
+
+        </Switch>
+      </Router>
+      </AuthContextProvider>
     </div>
       
   );
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 const Cart = (props) => {
     const cart = props.cart;
-    const total = cart.reduce((total,prd) => total + prd.price, 0);
+    //const total = cart.reduce((total,prd) => total + prd.price, 0);
+    let total = 0;
+    for(let i=0; i<cart.length; i++){
+        const product = cart[i];
+        total = total + product.price*product.quantity;
+        
+    }
     
     let shipping = 0;
     if(total>35){
@@ -27,6 +33,10 @@ const Cart = (props) => {
             <p><small>Shipping Cost: {shipping}</small></p>
             <p><small>Tax + Vat: {tax}</small></p>
             <p>Total price: {grandTotal}</p>
+            <br/>
+            {
+                props.children
+            }
         </div>
     );
 };
